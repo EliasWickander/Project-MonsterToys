0c17a8cfff470be4d219c28b1bf38a14e6c28466
Elias Wickander <elias.wt@hotmail.com>
2022-02-06 22:10:42 +0100

    Revert "Implemented astar pathfinding and started work on cinematics/camera"
    This reverts commit c1e34aaa517ee64edf4ec6a2c3757c2f39c30ec0.

--- Assets/Scripts/Utils/Pathfinding/NavNode.cs
+++ Assets/Scripts/Utils/Pathfinding/NavNode.cs
@@ -2,16 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridNode
+public class NavNode
 {
     public bool m_obstacle;
     public Vector3 m_worldPos;
 
-    public int gCost;
-    public int hCost;
-    public int FCost => gCost + hCost;
-
-    public GridNode(Vector3 worldPos, bool obstacle)
+    public NavNode(Vector3 worldPos, bool obstacle)
     {
         m_obstacle = obstacle;
         m_worldPos = worldPos;
