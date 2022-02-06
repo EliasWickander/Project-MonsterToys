c1e34aaa517ee64edf4ec6a2c3757c2f39c30ec0
Elias Wickander <elias.wt@hotmail.com>
2022-02-06 22:10:42 +0100

    Implemented astar pathfinding and started work on cinematics/camera

--- Assets/Scripts/Utils/Pathfinding/NavNode.cs
+++ Assets/Scripts/Utils/Pathfinding/NavNode.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NavNode
+public class GridNode
 {
     public bool m_obstacle;
     public Vector3 m_worldPos;
 
-    public NavNode(Vector3 worldPos, bool obstacle)
+    public int gCost;
+    public int hCost;
+    public int FCost => gCost + hCost;
+
+    public GridNode(Vector3 worldPos, bool obstacle)
     {
         m_obstacle = obstacle;
         m_worldPos = worldPos;
